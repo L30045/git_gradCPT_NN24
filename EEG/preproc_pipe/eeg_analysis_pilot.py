11e0c7dd9b501efcfdd41e40a170152d6c834f58
L30045 <chiyuan30045@gmail.com>
2025-10-21 14:20:14 -0400

    ignore pycache

--- EEG/preproc_pipe/eeg_analysis_pilot.py
+++ EEG/preproc_pipe/eeg_analysis_pilot.py
@@ -44,30 +44,8 @@ EEG = eeg_preproc_basic(EEG, is_bpfilter=is_bpfilter, bp_f_range=bp_f_range,
 # load corresponding event file
 event_file = os.path.join(data_path,os.pardir,f"sub-{subj_id}","nirs",
                           f"sub-{subj_id}_task-gradCPT_run-{run_id:02d}_events.tsv")
-events_df = pd.read_csv(event_file,sep='\t')
-event_duration = float(events_df["duration"].values[0])
-is_event_correct = (events_df["value"].values).astype(int)
-is_event_mnt = ((events_df["trial_type"]=="mnt").values).astype(int)
-event_ids = is_event_mnt*2+is_event_correct
-event_labels_lookup = dict(city_incorrect=0, city_correct=1,
-                           mnt_incorrect=2, mnt_correct=3)
-# select event to epoch
-
-# create events array (onset, stim_channel_voltage, event_id)
-events = np.column_stack(((events_df["onset"]*EEG.info["sfreq"]).astype(int),
-                    np.zeros(len(events_df), dtype=int),
-                    event_ids))
-n_select_ev = np.sum(events[:,-1]==event_labels_lookup[select_event])
-print(f"# {select_event}/ # total = {n_select_ev}/{events.shape[0]} ({n_select_ev/events.shape[0]*100:.1f}%)")
-# pick only selected event
-events = events[events[:,-1]==event_labels_lookup[select_event]]
-# epoch by event
-epochs = mne.Epochs(EEG, events=events,event_id={select_event:event_labels_lookup[select_event]},preload=True,
-                    tmin=baseline_length, tmax=event_duration+baseline_length,
-                    reject=epoch_reject_crit,
-                    detrend=is_detrend, 
-                    )
-print(f"# Epochs below PTP threshold ({epoch_reject_crit['eeg']*1e6} uV) = {len(epochs.selection)}")
+epochs = epoch_by_select_event(EEG, event_file, select_event=select_event,baseline_length=baseline_length,
+                                                epoch_reject_crit=dict(eeg=100e-6), is_detrend=1)
 
 #%% Visualizing
 vis_ch = 'fz'
--- EEG/preproc_pipe/utils.py
+++ EEG/preproc_pipe/utils.py
@@ -65,3 +65,30 @@ def eeg_preproc_basic(EEG, is_bpfilter=True, bp_f_range=[0.1, 45],
         EEG = ica.apply(EEG)
 
     return EEG
+
+
+def epoch_by_select_event(EEG, event_file, select_event='mnt_correct',baseline_length=-0.2,epoch_reject_crit=dict(eeg=100e-6), is_detrend=1):
+    events_df = pd.read_csv(event_file,sep='\t')
+    event_duration = float(events_df["duration"].values[0])
+    is_event_correct = (events_df["value"].values).astype(int)
+    is_event_mnt = ((events_df["trial_type"]=="mnt").values).astype(int)
+    event_ids = is_event_mnt*2+is_event_correct
+    event_labels_lookup = dict(city_incorrect=0, city_correct=1,
+                            mnt_incorrect=2, mnt_correct=3)
+    # create events array (onset, stim_channel_voltage, event_id)
+    events = np.column_stack(((events_df["onset"]*EEG.info["sfreq"]).astype(int),
+                        np.zeros(len(events_df), dtype=int),
+                        event_ids))
+    n_select_ev = np.sum(events[:,-1]==event_labels_lookup[select_event])
+    print(f"# {select_event}/ # total = {n_select_ev}/{events.shape[0]} ({n_select_ev/events.shape[0]*100:.1f}%)")
+    # pick only selected event
+    events = events[events[:,-1]==event_labels_lookup[select_event]]
+    # epoch by event
+    epochs = mne.Epochs(EEG, events=events,event_id={select_event:event_labels_lookup[select_event]},preload=True,
+                        tmin=baseline_length, tmax=event_duration+baseline_length,
+                        reject=epoch_reject_crit,
+                        detrend=is_detrend, 
+                        )
+    print(f"# Epochs below PTP threshold ({epoch_reject_crit['eeg']*1e6} uV) = {len(epochs.selection)}")
+
+    return epochs
