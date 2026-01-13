6e5c4f903e0449cbbde254317b4c3f66cd6bb167
L30045 <chiyuan30045@gmail.com>
2026-01-13 17:20:13 -0500

    func for ERP area and trial idx

--- modeling/model.py
+++ modeling/model.py
@@ -135,3 +135,146 @@ def prune_mask_ts(ts, pruned_chans):
 
     ts_masked = ts.where(~mask_expanded, np.nan)
     return ts_masked
+
+def find_zero_crossings(signal, times):
+    """
+    Find zero crossing indices in a signal.
+
+    Parameters:
+    -----------
+    signal : array
+        The signal data
+    times : array
+        Time vector corresponding to the signal
+
+    Returns:
+    --------
+    zero_crossing_times : array
+        Times where zero crossings occur
+    zero_crossing_indices : array
+        Indices where zero crossings occur
+    """
+    # Find sign changes (zero crossings)
+    sign_changes = np.diff(np.sign(signal))
+    zero_crossing_indices = np.where(sign_changes != 0)[0]
+
+    # Interpolate to get exact crossing times
+    zero_crossing_times = []
+    for idx in zero_crossing_indices:
+        # Linear interpolation to find exact zero crossing
+        t1, t2 = times[idx], times[idx + 1]
+        v1, v2 = signal[idx], signal[idx + 1]
+        # t_zero = t1 - v1 * (t2 - t1) / (v2 - v1)
+        t_zero = t1 + (0 - v1) * (t2 - t1) / (v2 - v1)
+        zero_crossing_times.append(t_zero)
+
+    return np.array(zero_crossing_times), zero_crossing_indices
+
+def calculate_component_area(signal, times, start_idx, end_idx):
+    """
+    Calculate the area under a component using trapezoidal integration.
+
+    Parameters:
+    -----------
+    signal : array
+        The signal data
+    times : array
+        Time vector corresponding to the signal
+    start_idx : int
+        Start index of the component
+    end_idx : int
+        End index of the component
+
+    Returns:
+    --------
+    area : float
+        Area under the curve (can be negative)
+    """
+    return np.trapz(signal[start_idx:end_idx+1], times[start_idx:end_idx+1])
+
+def extract_n2_p3_features(signal, times, n2_window=(0.4, 0.7), p3_window=(0.6, 1.1)):
+    """
+    Extract N2 and P3 features from ERP signal.
+
+    Parameters:
+    -----------
+    signal : array
+        The ERP signal
+    times : array
+        Time vector in seconds
+    n2_window : tuple
+        Time window (start, end) in seconds to search for N2 peak
+    p3_window : tuple
+        Time window (start, end) in seconds to search for P3 peak
+
+    Returns:
+    --------
+    results : dict
+        Dictionary containing:
+        - zero_crossing_times: all zero crossing time points
+        - n2_peak_time: time of N2 peak
+        - n2_peak_amp: amplitude of N2 peak
+        - n2_area: area of N2 component
+        - p3_peak_time: time of P3 peak
+        - p3_peak_amp: amplitude of P3 peak
+        - p3_area: area of P3 component
+    """
+    # Find all zero crossings
+    zero_crossing_times, zero_crossing_indices = find_zero_crossings(signal, times)
+
+    # Find N2 (negative peak in specified window)
+    n2_mask = (times >= n2_window[0]) & (times <= n2_window[1])
+    n2_idx = np.argmin(signal[n2_mask])
+    n2_global_idx = np.where(n2_mask)[0][n2_idx]
+    n2_peak_time = times[n2_global_idx]
+    n2_peak_amp = signal[n2_global_idx]
+
+    # Find N2 boundaries (zero crossings around N2 peak)
+    n2_start_crossings = zero_crossing_indices[zero_crossing_indices < n2_global_idx]
+    n2_end_crossings = zero_crossing_indices[zero_crossing_indices > n2_global_idx]
+
+    if len(n2_start_crossings) > 0 and len(n2_end_crossings) > 0:
+        n2_start_idx = n2_start_crossings[-1]
+        n2_end_idx = n2_end_crossings[0]
+        n2_area = calculate_component_area(signal, times, n2_start_idx, n2_end_idx)
+    else:
+        n2_area = np.nan
+        n2_start_idx = None
+        n2_end_idx = None
+
+    # Find P3 (positive peak in specified window)
+    p3_mask = (times >= p3_window[0]) & (times <= p3_window[1])
+    p3_idx = np.argmax(signal[p3_mask])
+    p3_global_idx = np.where(p3_mask)[0][p3_idx]
+    p3_peak_time = times[p3_global_idx]
+    p3_peak_amp = signal[p3_global_idx]
+
+    # Find P3 boundaries (zero crossings around P3 peak)
+    p3_start_crossings = zero_crossing_indices[zero_crossing_indices < p3_global_idx]
+    p3_end_crossings = zero_crossing_indices[zero_crossing_indices > p3_global_idx]
+
+    if len(p3_start_crossings) > 0 and len(p3_end_crossings) > 0:
+        p3_start_idx = p3_start_crossings[-1]
+        p3_end_idx = p3_end_crossings[0]
+        p3_area = calculate_component_area(signal, times, p3_start_idx, p3_end_idx)
+    else:
+        p3_area = np.nan
+        p3_start_idx = None
+        p3_end_idx = None
+
+    results = {
+        'zero_crossing_times': zero_crossing_times,
+        'zero_crossing_indices': zero_crossing_indices,
+        'n2_peak_time': n2_peak_time,
+        'n2_peak_amp': n2_peak_amp,
+        'n2_area': n2_area,
+        'n2_start_idx': n2_start_idx,
+        'n2_end_idx': n2_end_idx,
+        'p3_peak_time': p3_peak_time,
+        'p3_peak_amp': p3_peak_amp,
+        'p3_area': p3_area,
+        'p3_start_idx': p3_start_idx,
+        'p3_end_idx': p3_end_idx
+    }
+
+    return results
---
+++ modeling/params_setting.py
@@ -0,0 +1,103 @@
+#%% libraries
+import sys
+from tqdm import tqdm
+import statsmodels.api as sm
+from patsy import dmatrices
+from scipy.optimize import fmin
+from sklearn.decomposition import PCA
+from functools import reduce
+import operator
+
+##############################################################################
+import gzip
+import re 
+import xarray as xr
+
+import cedalion
+import cedalion.nirs
+from cedalion import units
+import cedalion.models.glm as glm
+from cedalion.sigproc import quality
+from cedalion import units
+import cedalion.sigproc.motion_correct as motion
+from cedalion.plots import scalp_plot
+from scipy.signal import filtfilt, windows
+import cedalion.sigproc.frequency as frequency
+
+# import my own functions from a different directory
+sys.path.append('/projectnb/nphfnirs/s/users/lcarlton/ANALYSIS_CODE/cedalion_help_funcs/')
+import processing_func as pf
+import image_recon_func as irf
+
+sys.path.append('/projectnb/nphfnirs/s/users/lcarlton/ANALYSIS_CODE/cedalion-pipeline/workflow/scripts/modules')
+import module_preprocess as mpf
+
+#%% path setting
+project_path = '/projectnb/nphfnirs/s/datasets/gradCPT_NN24/'
+
+
+
+#%%
+ch_names = ['fz','cz','pz','oz']
+is_bpfilter = True
+bp_f_range = [0.1, 45] #band pass filter range (Hz)
+is_reref = True
+reref_ch = ['tp9h','tp10h']
+is_ica_rmEye = True
+baseline_length = -0.2
+epoch_reject_crit = dict(
+                        eeg=100e-6 #unit:V
+                        )
+is_detrend = 1 # 0:constant, 1:linear, None
+
+preproc_params = dict(
+    is_bpfilter = is_bpfilter,
+    bp_f_range = bp_f_range,
+    is_reref = is_reref,
+    reref_ch = reref_ch,
+    is_ica_rmEye = is_ica_rmEye,
+    baseline_length = baseline_length,
+    epoch_reject_crit = epoch_reject_crit,
+    is_detrend = is_detrend,
+    ch_names = ch_names,
+    is_overwrite = False
+)
+
+#%% GLM setup 
+RUN_PREPROCESS = True
+RUN_HRF_ESTIMATION = True
+SPLIT_VTC = False
+SAVE_RESIDUAL = False
+NOISE_MODEL = 'ar_irls'
+root_dir = "/projectnb/nphfnirs/s/datasets/gradCPT_NN24/"
+
+if NOISE_MODEL == 'ols':
+    DO_TDDR = True
+    DO_DRIFT = True
+    DO_DRIFT_LEGENDRE = False
+    DRIFT_ORDER = 3
+    F_MIN = 0 * units.Hz
+    F_MAX = 0.5 * units.Hz
+elif NOISE_MODEL == 'ar_irls':
+    DO_TDDR = False
+    DO_DRIFT = False
+    DO_DRIFT_LEGENDRE = True
+    DRIFT_ORDER = 3
+    F_MAX = 0
+    F_MIN = 0
+else:
+    print('Not a valid noise model - please select ols or ar_irls')
+
+cfg_GLM = {
+    'do_drift': DO_DRIFT,
+    'do_drift_legendre': DO_DRIFT_LEGENDRE,
+    'do_short_sep': True,
+    'drift_order' : DRIFT_ORDER,
+    'distance_threshold' : 20*units.mm, # for ssr
+    'short_channel_method' : 'mean',
+    'noise_model' : NOISE_MODEL,
+    't_delta' : 1*units.s ,   # for seq of Gauss basis func - the temporal spacing between consecutive gaussians
+    't_std' : 1*units.s ,  
+    't_pre' : 2*units.s,
+    't_post' : 10*units.s
+    }
