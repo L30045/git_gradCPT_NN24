fb12ecc07a07dbe886ebdf93e6de08767d06385e
L30045 <chiyuan30045@gmail.com>
2026-03-17 10:06:33 -0400

    cross subjects analysis for pupil size

--- modeling/run_model_EEG_inform.py
+++ modeling/run_model_EEG_inform.py
@@ -18,7 +18,7 @@ from params_setting import *
 from tqdm import tqdm
 
 #%%
-subj_id_array = [695,721,723,726, 730]
+subj_id_array = [670, 695,721,723,726, 730]
 model_type='full_noEEG_rejected_ttest'
 # subj_id_array =[730]
 # subj_id_array = [670, 671, 673, 695, 719, 721, 723, 726, 727, 730, 733]
