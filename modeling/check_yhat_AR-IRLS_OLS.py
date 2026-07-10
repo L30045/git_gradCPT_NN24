bdb43fdf3727e02b9b45eee31d3bca9ca81b96e7
L30045 <chiyuan30045@gmail.com>
2026-07-10 10:39:00 -0400

    Check OLS results

--- modeling/check_yhat_AR-IRLS_OLS.py
+++ modeling/check_yhat_AR-IRLS_OLS.py
@@ -26,64 +26,6 @@ import xarray as xr
 import cedalion.xrutils as xrutils
 import copy
 
-#%% settings
-subj_id = 723
-debug_channel = 'S10D127'
-debug_chromo = 'HbO'
-save_file_path = os.path.join(project_path, 'derivatives', 'eeg', f"sub-{subj_id}")
-
-#%% load design matrices and dependent variable (Y_all, as in model_EEG_inform_single_subject.py)
-with open(os.path.join(save_file_path, 'dm_dict.pkl'), 'rb') as f:
-    dm_dict = pickle.load(f)
-
-Y_all = dm_dict['Y_all']  # dims: chromo, channel, time
-y_true = Y_all.sel(channel=[debug_channel])
-
-#%% train model using my_ar_irls_GLM
-# stim only model
-stim_results, stim_ar = model.my_fit(y_true, dm_dict['onlyStim'], autoReg=None)
-betas = stim_results.sm.params
-y_hat = (dm_dict['onlyStim'].common * betas).sum('regressor')
-y_hat_my_stim = y_hat.transpose('chromo', 'channel', 'time')
-
-#%% Using AR from Full
-full_results, full_ar = model.my_fit(y_true, dm_dict['full'], autoReg=None)
-betas = full_results.sm.params
-y_hat = (dm_dict['full'].common * betas).sum('regressor')
-y_hat_full = y_hat.transpose('chromo', 'channel', 'time')
-
-stim_results, _ = model.my_fit(y_true, dm_dict['onlyStim'], autoReg=full_ar)
-betas = stim_results.sm.params
-y_hat = (dm_dict['onlyStim'].common * betas).sum('regressor')
-y_hat_my_stim_full_ar = y_hat.transpose('chromo', 'channel', 'time')
-
-#%% compare stim-only, EEG-only, and full model y_hat for a single channel
-plt.figure(figsize=(14, 4))
-plt.plot(Y_all.time.values, y_true.sel(chromo='HbO').values.flatten(), label='Y (true)', color='k', linewidth=1)
-
-y_pred = y_hat_my_stim.sel(channel=debug_channel, chromo=debug_chromo).values
-plt.plot(Y_all.time.values, y_pred, label='My Stim', alpha=0.7)
-
-y_pred = y_hat_ori_stim.sel(channel=debug_channel, chromo=debug_chromo).values
-plt.plot(Y_all.time.values, y_pred, label='Cedalion Stim', alpha=0.7)
-
-y_pred = y_hat_full.sel(channel=debug_channel, chromo=debug_chromo).values
-plt.plot(Y_all.time.values, y_pred, label='My Full', alpha=0.7)
-
-
-y_pred = y_hat_my_stim_full_ar.sel(channel=debug_channel, chromo=debug_chromo).values
-plt.plot(Y_all.time.values, y_pred, label='Full-AR Stim', alpha=0.7)
-
-
-plt.xlabel('time (s)')
-plt.ylabel(f'{debug_chromo} concentration')
-plt.title(f'sub-{subj_id} channel={debug_channel}')
-plt.legend()
-plt.grid()
-plt.tight_layout()
-plt.show()
-
-
 #%%
 subj_id = 723
 debug_channel = 'S10D127'
@@ -564,4 +506,50 @@ for ax_i in range(3):
     axs[ax_i].legend()
     axs[ax_i].grid()
 
+#%% Check cedalion.math.ar_irls.py ar_irls_GLM
+ts = Y_test
+stim_dm = model.get_GLM_copy_from_pf_DM(run_list, cfg_GLM, cfg_GLM['geo3d'], pruned_chans_list, stim_list)
+stim_dm.common = stim_dm.common.sel(chromo=['HbO'])
+design_matrix = stim_dm
+verbose = False
 
+ts = ts.pint.dequantify()
+dim3_name = xrutils.other_dim(design_matrix.common, "time", "regressor")
+reg_results = xr.DataArray(
+np.empty((ts.sizes["channel"], ts.sizes[dim3_name]), dtype=object),
+dims=("channel", dim3_name),
+coords=xrutils.coords_from_other(ts.isel(time=0), dims=("channel", dim3_name))
+)
+
+for (
+dim3,
+group_channels,
+group_design_matrix,
+) in design_matrix.iter_computational_groups(ts):
+        group_y = ts.sel({"channel": group_channels, dim3_name: dim3}).transpose(
+                "time", "channel"
+        )
+        # pass x as a DataFrame to statsmodel to make it aware of regressor names
+        x = pd.DataFrame(
+                group_design_matrix.values, columns=group_design_matrix.regressor.values
+        )
+        for chan in tqdm(group_y.channel.values, disable=not verbose):
+                result = cedalion.math.ar_irls.ar_irls_GLM(group_y.loc[:, chan], x)
+                reg_results.loc[chan, dim3] = result
+
+description = 'Cedalion'
+reg_results.attrs["description"] = description
+
+betas = reg_results.sm.params
+y_hat = (design_matrix.common * betas).sum('regressor')
+y_hat_stim_cedalion = y_hat.transpose('chromo', 'channel', 'time')
+
+fig, axs = plt.subplots(1, 1, figsize=(14, 4), sharex=True)
+
+axs.plot(ts.time.values, ts.values.flatten(), label='Y (true)', color='k', linewidth=1)
+axs.plot(ts.time.values, y_hat_stim_cedalion.values.flatten(),
+        label='Stim only (No HPF)', alpha=0.7)
+axs.set_ylabel(f'{debug_chromo} concentration')
+axs.set_title(f'Cedalion (AR-IRLS)')
+axs.legend()
+axs.grid()
